bc1fbf2d628136be818fe6926cf2816198b01608
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-09-15 20:47:56 -0300

    Comentários - Atalhos

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            Console.WriteLine("O C# tem o XML Comments");
+            /*
+             * Comentários 
+             * de Múltiplas
+             * Linhas
+             */
+            //Console.WriteLine("Comenta com Atalho CTRL + K + C");
+            Console.WriteLine("Descomenta com Atalho CTRL + K + U");
+        }
+    }
+}
