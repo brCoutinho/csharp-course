fa266bee43924dd92f2689abc1b13d6d379b7bc1
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-09-15 21:23:04 -0300

    Vaŕiáveis e Constantes

---
+++ CursoCSharp/Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class VariaveisEConstantes
+    {
+        public static void Executar()
+        {
+            //area da circunferência
+            double raio = 4.5;
+            const double PI = 3.14;
+
+            raio = 5.5;
+            //PI = 3.1412;
+
+            double area = PI * raio * raio;
+
+            Console.WriteLine("A área é: " + area);
+
+            //Tipos Internos
+
+            bool areaGrande = true;
+
+            Console.WriteLine("A área é Grande? " + areaGrande);
+
+            byte idade = 45;
+
+            sbyte saldoDeGols = sbyte.MinValue;//byte com sinal de negativo
+            Console.WriteLine("Saldo de Gols: " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("Salário " + salario);
+
+            int menorValorInt = int.MinValue;
+            Console.WriteLine("Menor Inteiro " + menorValorInt);
+
+            uint populacaoBrasileira = 207_600_000;
+            Console.WriteLine("População " + populacaoBrasileira);
+
+            float precoComputador = 12999.99f;
+
+            decimal distanciaEntreEstrelas = decimal.MaxValue;
+            Console.WriteLine("Espaço entre as estrelas " + distanciaEntreEstrelas);
+
+            char letra = 'a';
+            string texto = "Curso de C#";
+                
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -9,6 +9,7 @@ namespace CursoCSharp {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
+                {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},// CTRL + D - Replica linha
             });
 
             central.SelecionarEExecutar();
