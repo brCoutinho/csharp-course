0f32cb082b0a5c8ad3f4640aa9ba95ffba37d6b4
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-09-16 20:32:55 -0300

    Inferencia

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Inferencia
+    {
+        public static void Executar()
+        {
+            var nome = "Thiago";//Atribuir a uma variável um tipo sem que tenha sido definido esse tipo
+            Console.WriteLine(nome);
+
+        }
+    }
+}
