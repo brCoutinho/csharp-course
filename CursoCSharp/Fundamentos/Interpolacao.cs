617e10ebd1442de37ac50218de5a9cb1c55b6b79
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-09-16 20:33:38 -0300

    Interpolação

---
+++ CursoCSharp/Fundamentos/Interpolacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Interpolacao
+    {
+        public static void Executar()
+        {
+            string nome = "Notebook Aspire 5";
+            string marca = "Acer";
+            double valor = 3799.00;
+
+            Console.WriteLine("O {0} da marca {1} custa {2}.",nome,marca,valor);
+
+            Console.WriteLine($"A marca {marca} é legal");
+            Console.WriteLine($"1 + 1 = {1 + 1}");
+        }
+    }
+}
