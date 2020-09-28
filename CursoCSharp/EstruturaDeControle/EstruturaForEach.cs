1091f833044e9df07f1192c9add50717dab18a9f
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-09-27 22:35:27 -0300

    Estrutura Foreach

---
+++ CursoCSharp/EstruturaDeControle/EstruturaForEach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    class EstruturaForEach
+    {
+        public static void Executar()
+        {
+            var palavra = "Problemático";
+
+            foreach(var letra in palavra)
+            {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new String[]
+            {"Amanda","Thiago","Lucas","Jairo","Hernanes"};
+
+            foreach(var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+    }
+}
