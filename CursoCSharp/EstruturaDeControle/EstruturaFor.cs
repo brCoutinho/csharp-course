7674cf137c760d2a4b702d673da863bbc5a7a731
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-09-27 22:23:31 -0300

    Estrutura For

---
+++ CursoCSharp/EstruturaDeControle/EstruturaFor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    class EstruturaFor
+    {
+        public static void Executar()
+        {
+            //declaração da variável; expressão(true/false); incremento na variável
+            //for(int i = 1;  i <= 10; i++)
+            //{
+            //    Console.WriteLine($"O valor de i é: {i}");
+            //}
+
+            double somatorio = 0;
+            string entrada;
+
+            Console.Write("Informe o tamanho da turma: ");
+
+            entrada = Console.ReadLine();
+            int.TryParse(entrada, out int tamanhoTurma);
+
+            for(int i = 1; i <= tamanhoTurma; i++)
+            {
+                Console.Write($"Informe a nota do aluno {i}: ");
+                entrada = Console.ReadLine();
+                double.TryParse(entrada, out double notaAtual);
+
+                somatorio += notaAtual;
+            }
+
+            double media = tamanhoTurma > 0 ? somatorio / tamanhoTurma : 0;
+            Console.WriteLine($"A média da turma é: {media}");
+        }
+    }
+}
