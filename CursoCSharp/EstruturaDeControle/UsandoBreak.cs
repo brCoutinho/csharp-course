63a4d17f9d1f88be3f6e4086308417fe92215f14
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-09-27 22:57:22 -0300

    Usando break

---
+++ CursoCSharp/EstruturaDeControle/UsandoBreak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    class UsandoBreak
+    {
+        public static void Executar()
+        {
+            Random random = new Random();
+            int numero = random.Next(1, 51);
+
+            Console.WriteLine($"O número sorteado foi {numero}");
+
+            for(int i =1; i <= 50; i++)
+            {
+                Console.Write($"{i} É o número que queremos? ");
+                if(i == numero)
+                {
+                    Console.WriteLine("Sim");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Não");
+                }
+            }
+
+            Console.WriteLine("Fim!");
+
+        }
+    }
+}
