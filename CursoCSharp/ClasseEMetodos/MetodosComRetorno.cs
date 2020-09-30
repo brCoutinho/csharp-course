d8d32ea795901b3fd6e13bf8d48d277970f29bc5
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-09-29 21:27:57 -0300

    Métodos com Retorno

---
+++ CursoCSharp/ClasseEMetodos/MetodosComRetorno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClasseEMetodos
+{
+    class CalculadoraComum
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+    }
+    class MetodosComRetorno
+    {
+        public static void Executar()
+        {
+            var calculadoraComum = new CalculadoraComum();
+            var resultado = calculadoraComum.Somar(3, 7);
+
+            Console.WriteLine(resultado);
+            Console.WriteLine(calculadoraComum.Subtrair(6,18));
+            Console.WriteLine(calculadoraComum.Multiplicar(7,7));
+
+        }
+    }
+}
